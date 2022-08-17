948d7ba6bc1fee34f5429b74cbc7d995646bda60
Morpa <andremorpa@gmail.com>
2022-08-17 18:08:34 +0100

    add initial tests

--- src/app/shared/tab/tab.component.spec.ts
+++ src/app/shared/tab/tab.component.spec.ts
@@ -1,16 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { TabComponent } from './tab.component';
 
-describe('TabComponent', () => {
+describe('Tab Component', () => {
   let component: TabComponent;
   let fixture: ComponentFixture<TabComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TabComponent ]
-    })
-    .compileComponents();
+      declarations: [TabComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TabComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,22 @@ describe('TabComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have .hidden class', () => {
+    const element = fixture.debugElement.query(By.css('.hidden'));
+    const element2 = fixture.nativeElement.querySelector('.hidden');
+    const element3 = document.querySelector('.hidden');
+
+    expect(element).toBeTruthy();
+  });
+
+  it('should not have .hidden class', () => {
+    component.active = true;
+    fixture.detectChanges();
+
+    const element = fixture.debugElement.query(By.css('.hidden'));
+
+    expect(element).not.toBeTruthy();
   });
 });
--- src/app/shared/tabs-container/tabs-container.component.spec.ts
+++ src/app/shared/tabs-container/tabs-container.component.spec.ts
@@ -1,23 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { TabsContainerComponent } from './tabs-container.component';
+import { TabComponent } from '../tab/tab.component';
 
-describe('TabsContainerComponent', () => {
-  let component: TabsContainerComponent;
-  let fixture: ComponentFixture<TabsContainerComponent>;
+@Component({
+  template: `
+    <app-tabs-container>
+      <app-tab tabTitle="Tab 1">Tab 1</app-tab>
+      <app-tab tabTitle="Tab 2">Tab 2</app-tab>
+    </app-tabs-container>
+  `,
+})
+class TestHostComponent {}
+
+describe('TabsContainer Component', () => {
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TabsContainerComponent ]
-    })
-    .compileComponents();
+      declarations: [TabsContainerComponent, TabComponent, TestHostComponent],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(TabsContainerComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have two tabs', () => {
+    const tabs = fixture.debugElement.queryAll(By.css('li'));
+    const containerComponent = fixture.debugElement.query(
+      By.directive(TabsContainerComponent)
+    );
+    const tabsProps = containerComponent.componentInstance.tabs;
+
+    expect(tabs.length).toBe(2);
+    expect(tabsProps.length).toBe(2);
   });
 });
